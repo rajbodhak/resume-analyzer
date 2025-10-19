a4c1b6c7279e693a3b1b08d7bd0fb4008c8ac51d
Raj Bodhak <rajlaptop2004@gmail.com>
2025-10-19 13:44:11 +0530

    feat(layout): add NavbarWrapper component

---
+++ src/components/layout/components/NavbarWrapper.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import Navbar from "../Navbar";
+
+export default function NavbarWrapper() {
+    return (
+        <Navbar
+            logoText="Resume Analyzer"
+            logoHref="/"
+            signInText="Sign In"
+            ctaText="Get Started"
+            onSignInClick={() => {
+                window.location.href = '/signin';
+            }}
+            onCtaClick={() => {
+                window.location.href = '/upload';
+            }}
+        />
+    );
+}
