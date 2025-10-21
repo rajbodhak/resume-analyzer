12b2e9470b1160a3ce66af6162c06b7b4f20b0b2
Raj Bodhak <rajlaptop2004@gmail.com>
2025-10-21 13:42:00 +0530

    feat(home): add HowItWorks section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Features from "@/components/home/Features";
 import Hero from "@/components/home/Hero";
+import HowItWorks from "@/components/home/HowItWorks";
 
 export default function Home() {
   return (
     <div className="w-full">
       <Hero />
+      <HowItWorks />
       <Features />
     </div>
   );
