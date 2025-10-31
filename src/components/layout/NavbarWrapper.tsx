c75184c43663fcbed54cf80331afdafd4a3adc3f
Raj Bodhak <rajlaptop2004@gmail.com>
2025-10-31 23:19:49 +0530

    feat(navbar): Add dashboard link based on authentication

--- src/components/layout/NavbarWrapper.tsx
+++ src/components/layout/NavbarWrapper.tsx
@@ -1,16 +1,25 @@
 'use client';
 
+import { useAuthStore } from '@/lib/stores/auth-store';
 import Navbar from "./Navbar";
 
 export default function NavbarWrapper() {
+    const user = useAuthStore((state) => state.user);
+    const isAuthenticated = !!user;
+
     return (
         <Navbar
             logoText="Resume Analyzer"
             logoHref="/"
             signInText="Sign In"
+            dashboardText="Dashboard"
             ctaText="Get Started"
+            isAuthenticated={isAuthenticated}
             onSignInClick={() => {
                 window.location.href = '/auth/signin';
+            }}
+            onDashboardClick={() => {
+                window.location.href = '/dashboard';
             }}
             onCtaClick={() => {
                 window.location.href = '/upload';
@@ -18,4 +27,3 @@ export default function NavbarWrapper() {
         />
     );
 }
-
